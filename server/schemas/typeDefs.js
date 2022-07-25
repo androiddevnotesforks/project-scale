708e7839e7df0767db59e4d7f9e61a1e606a3857
r134x7 <r134x7@gmx.com>
2022-07-26 00:59:33 +1000

    added ambitions typedef and query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,22 +2,31 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type User {
-        _id: ID
-        firstName: String
-        lastName: String
-        username: String
-        email: String
+        _id: ID!
+        firstName: String!
+        lastName: String!
+        username: String!
+        email: String!
         # don't want to display password
     }
 
     type Category {
-        _id: ID
-        name: String
+        _id: ID!
+        name: String!
+    }
+
+    type Ambitions {
+        _id: ID!
+        name: String!
+        timeLimit: Int!
+        category: Category!
+        user: User!
     }
 
     type Query {
         user: User
         categories: [Category]
+        ambitions: [Ambitions]
     }
 
     type Mutation {
