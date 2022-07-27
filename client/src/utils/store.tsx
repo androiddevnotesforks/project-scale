5cb07d1460697b8ec745024f1e40a2dc4e431345
r134x7 <r134x7@gmx.com>
2022-07-27 15:11:07 +1000

    created redux toolkit store, placed provider at index.tsx

---
+++ client/src/utils/store.tsx
@@ -0,0 +1,9 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+// import reducers...
+
+// combine the reducers...
+
+export default configureStore({
+    // reducer: rootReducer
+})
