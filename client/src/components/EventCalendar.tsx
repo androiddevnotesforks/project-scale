eec5ec0f9af16b21668e0d773a7a440241d4ecd0
r134x7 <r134x7@gmx.com>
2022-08-01 13:47:08 +1000

    got the chart to display data

--- client/src/components/EventCalendar.tsx
+++ client/src/components/EventCalendar.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Calendar, isSameDate, DatePicker } from "@mantine/dates";
-import { Text, Indicator, Loader } from "@mantine/core";
+import { Text, Indicator, Loader, Space } from "@mantine/core";
 import { useQuery } from "@apollo/client";
 import { SEARCH_EVENTS } from "../utils/queries";
 import { useSelector } from "react-redux";
+import { Line } from "react-chartjs-2";
 
+import { Chart, registerables } from 'chart.js'; // required to actually get chart.js with react-chartjs-2 to work
+Chart.register(...registerables); // to get the package working, source: https://www.chartjs.org/docs/next/getting-started/integration.html
 
 export default function EventCalendar() {
     
@@ -52,7 +55,7 @@ export default function EventCalendar() {
             if (isSameDate(value || new Date(), new Date(date.createdAt))) {
                 return `Date: ${date.createdAt}, Data Input: ${date.dataInput}, Notes: ${date.notes}`
             } else {
-                return 0
+                return null
             }
         })
         
@@ -67,6 +70,8 @@ export default function EventCalendar() {
         }
     }));
     
+    // Chart data...
+
     
 
     // console.log(value);
@@ -115,9 +120,46 @@ export default function EventCalendar() {
                     )
                 }} 
             />
+            <Space />
             <Text >
                 {selectEvent()}
             </Text>
+            <Space />
+            <Line 
+                datasetIdKey="eventsChart"
+                data={{
+                    labels: viewRecords.map((data: any, index: any) => { // source for knowing index can be used as a parameter: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+                    
+                        return index + 1
+                    }),//array x-axis
+                    datasets: [
+                        {
+                            data: viewRecords.map((data: any) => {
+                                return data.dataInput
+                            }),
+                            label: "Lose Weight",
+                            borderColor: "crimson",
+
+                        },
+                    ], // hmmm
+                }}
+                options={{
+                    scales: {
+                        y: {
+                            title: {
+                              display: true,
+                              text: "Weight (kg)",
+                            },
+                          },
+                          x: {
+                            title: {
+                              display: true,
+                              text: "Days recorded",
+                            },
+                          },
+                    }
+                }}
+                 />
         </div>
         )}
 
